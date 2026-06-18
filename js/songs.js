f6bbdeba6e33a47140769722b501885097603c99
Kevin Sáenz <kevinsaenz503sv@gmail.com>
2026-06-17 19:51:52 -0600

    Update songs.js

---
+++ js/songs.js
@@ -0,0 +1,33 @@
+const songs = [
+    {
+        title: "Space Song",
+        artist: "Beach House",
+        file: "space_song.mp3",
+        cover: "space_song.jpg"
+    },
+    {
+        title: "Olvidarte",
+        artist: "Felipe Santos",
+        file: "olvidarte.mp3",
+        cover: "olvidarte.jpg"
+    },
+    {
+        title: "Cancioncitas",
+        artist: "Romeo Santos",
+        file: "cancioncitas.mp3",
+        cover: "cancioncitas.jpg"
+    }
+    {
+        title: "Rey sin reina",
+        artist: "Julión Álvarez y su Norteño Banda",
+        file: "rey-sin-reina.mp3",
+        cover: "rey-sin-reina.jpg"
+    }
+{
+        title: "Oye Traicionera",
+        artist: "Crooked Stilo",
+        file: "oye-traicionera.mp3",
+        cover: "traicionera.jpg"
+    }
+
+];
